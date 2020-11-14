c3a67eede679a759cba47a55336e7fe0bc26f23c
AntonChetveryk <chetverykanton92@gmail.com>
2020-11-14 16:18:55 +0200

    Updates

---
+++ precache-manifest.249f93b26f298e8932cfc22cd7cf2ac5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c386dae4239b5d286327185db51d273d",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "94d04d52e73b82d72de2",
+    "url": "/react_dynamic-list-of-posts/static/css/main.0c9de592.chunk.css"
+  },
+  {
+    "revision": "174797162dfa71ad866a",
+    "url": "/react_dynamic-list-of-posts/static/js/2.8641f3c6.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/react_dynamic-list-of-posts/static/js/2.8641f3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94d04d52e73b82d72de2",
+    "url": "/react_dynamic-list-of-posts/static/js/main.f77b2a1a.chunk.js"
+  },
+  {
+    "revision": "618358863681a9841d76",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime-main.d29b7207.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9b9b912321e3d315fc8f540d885eee7f.js"
+  "/react_dynamic-list-of-posts/precache-manifest.249f93b26f298e8932cfc22cd7cf2ac5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_dynamic-list-of-posts/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
