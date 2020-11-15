38ab151082383d57552806d793d47e5d44568cd8
AntonChetveryk <chetverykanton92@gmail.com>
2020-11-15 12:32:01 +0200

    Updates

--- precache-manifest.c55e37f3e4530cacbae4479255f817d1.js
+++ precache-manifest.c55e37f3e4530cacbae4479255f817d1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0b5a7fe6b9b4bcb57b1b98f23aca03e4",
+    "revision": "1f2bd80f97216b6dd65f05031b7f4baf",
     "url": "/react_dynamic-list-of-posts/index.html"
   },
   {
-    "revision": "560c22cf58f59456d770",
-    "url": "/react_dynamic-list-of-posts/static/css/main.0c9de592.chunk.css"
+    "revision": "159fab1c4480e4a5a469",
+    "url": "/react_dynamic-list-of-posts/static/css/main.4c39554a.chunk.css"
   },
   {
     "revision": "174797162dfa71ad866a",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react_dynamic-list-of-posts/static/js/2.8641f3c6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "560c22cf58f59456d770",
-    "url": "/react_dynamic-list-of-posts/static/js/main.e099d253.chunk.js"
+    "revision": "159fab1c4480e4a5a469",
+    "url": "/react_dynamic-list-of-posts/static/js/main.5097626a.chunk.js"
   },
   {
     "revision": "618358863681a9841d76",
